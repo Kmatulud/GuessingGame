daf2f711ea65fb5b69d9b6191041be948a3b513a
kmatulud <princessmakgotso@gmail.com>
2022-02-24 12:48:38 +0200

    Added some more error messages

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let guessElem = document.querySelector(".guessBtn");
 let input = document.querySelector(".userInput");
 let message = document.querySelector(".msg");
+message.style.color = "green";
+
 
 let randomNumber = Math.ceil((Math.random() * 100));
 
@@ -8,25 +10,34 @@ message.innerHTML = "New game started!";
 
 setTimeout(()=>{
     message.innerHTML = "";
-    message.style.color = "green";
 }, 3000);
+
 
 function getRandomNum(){
 
-    let inputElem  = input.value;
+    let inputElem  = Number(input.value);
 
-    if(inputElem > randomNumber){
-        message.innerHTML = "Your guess is too high";
+    if(inputElem < 1 || inputElem > 100 || inputElem === ""){
+        message.innerHTML = "Invalid ..should be numbers between 1 and 100";
     }
     else if(inputElem < randomNumber){
         message.innerHTML = "Your guess is too low";
     }
+    else if(inputElem > randomNumber){
+        message.innerHTML = "Your guess is too high";
+    }
+    
     else{
         message.innerHTML = `Correct, the secret number is ${randomNumber}`;
+
+        setTimeout(()=>{
+            location.reload();
+            // randomNumber = Math.ceil((Math.random() * 100));
+            // message.innerHTML = "New game started";
+        }, 5000);
     }
     setTimeout(()=>{
         message.innerHTML = "";
-        message.style.color = "green";
     }, 3000);
 }
 
